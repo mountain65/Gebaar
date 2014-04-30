d2105c297849730718fac89d5b7f9c84be9a2912
Richard de Zwart <richard@dezwartenco.nl>
2014-04-30 20:31:54 +0200

    Selects al the signs starting with the search string

--- Gebaar/Program.cs
+++ Gebaar/Program.cs
@@ -15,29 +15,33 @@ namespace Gebaar
 			var iniReader = new IniReader(path);
 
 			var begrippen = iniReader.Read("begrip_ok.ini").ToDictionary(l => l[1].Split (';').First (), l => l[0]);
+			var gebaren = iniReader.Read("gebaar_ok.ini").ToDictionary(l => l[1].Split(';').First (), l => l[1]);
+			var films = iniReader.Read("film_ok.ini").ToDictionary(l => l[0], l => l[1]);
 
 			var begripToFind = args [0].ToUpper();
 			if (begripToFind == "-ALL") {
 				DisplayList (begrippen);
 			}
 
-			if (!begrippen.ContainsKey (begripToFind)) {
+			var matches = begrippen.Where (b => b.Key.StartsWith (begripToFind));
+			if (matches.Count() == 0) {
 				Console.WriteLine ("Begrip {0} niet gevonden", begripToFind);
 				return;
 			}
 
-			var begripNr = begrippen [begripToFind];
+			foreach (var match in matches) {
 
-			var gebaren = iniReader.Read("gebaar_ok.ini").ToDictionary(l => l[1].Split(';').First (), l => l[1]);
-			if (!gebaren.ContainsKey (begripNr)) {
-				Console.WriteLine ("Gebaar {0} niet gevonden bij begrip {1}", begripNr, begripToFind);
-				return;
+				var begripNr = begrippen [match.Key];
+
+				if (!gebaren.ContainsKey (begripNr)) {
+					Console.WriteLine ("Gebaar {0} niet gevonden bij begrip {1}", begripNr, begripToFind);
+					return;
+				}
+
+				var filmNr = gebaren [begripNr].Split (';').Last ();
+
+				Process.Start("/Applications/QuickTime Player.app", Path.Combine(path, "films", films[filmNr] + ".mpg"));
 			}
-
-			var filmNr = gebaren [begripNr].Split (';').Last ();
-			var films = iniReader.Read("film_ok.ini").ToDictionary(l => l[0], l => l[1]);
-
-			Process.Start("/Applications/QuickTime Player.app", Path.Combine(path, "films", films[filmNr] + ".mpg"));
 		}
 
 
