671b2aab8c3db3a7bb72726a1043168fc970f741
Richard de Zwart <richard@dezwartenco.nl>
2014-04-30 20:19:06 +0200

    Cleaned up the code

--- Gebaar/Program.cs
+++ Gebaar/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace Gebaar
 {
@@ -9,15 +10,14 @@ namespace Gebaar
 	{
 		public static void Main (string[] args)
 		{
-			var path = ".";
+//			var path = ".";
+			var iniReader = new IniReader("/Users/mountain/Public/GebarenDVDs/Sexualiteit");
 
-			var begrippen = File.ReadAllLines (Path.Combine(path, "begrip_ok.ini")).SkipWhile(w => w.Trim () != "[Data]").Skip (1).Select (b => b.Split('=')).ToDictionary(l => l[1].Split (';').First (), l => l[0]);
+			var begrippen = iniReader.Read("begrip_ok.ini").ToDictionary(l => l[1].Split (';').First (), l => l[0]);
 
 			var begripToFind = args [0].ToUpper();
 			if (begripToFind == "-ALL") {
-				foreach(var begrip in begrippen.OrderBy(b => b.Key))
-					Console.WriteLine(begrip.Key);
-				return;
+				DisplayList (begrippen);
 			}
 
 			if (!begrippen.ContainsKey (begripToFind)) {
@@ -25,20 +25,40 @@ namespace Gebaar
 				return;
 			}
 
-			var gebaren = File.ReadAllLines (Path.Combine(path, "gebaar_ok.ini")).SkipWhile(w => w != "[Data]").Skip (1).Select (b => b.Split('=')).ToDictionary(l => l[1].Split(';').First (), l => l[1]);
 			var begripNr = begrippen [begripToFind];
 
+			var gebaren = iniReader.Read("gebaar_ok.ini").ToDictionary(l => l[1].Split(';').First (), l => l[1]);
 			if (!gebaren.ContainsKey (begripNr)) {
 				Console.WriteLine ("Gebaar {0} niet gevonden bij begrip {1}", begripNr, begripToFind);
 				return;
 			}
 
 			var filmNr = gebaren [begripNr].Split (';').Last ();
-			var films = File.ReadAllLines (Path.Combine(path, "film_ok.ini")).SkipWhile(w => w != "[Data]").Skip (1).Select (b => b.Split('=')).ToDictionary(l => l[0], l => l[1]);
-
-			Console.WriteLine ("Filmbestand={0}", Path.Combine(path, "films", films[filmNr] + ".mpg"));
+			var films = iniReader.Read(path, "film_ok.ini").ToDictionary(l => l[0], l => l[1]);
 
 			Process.Start("/Applications/QuickTime Player.app", Path.Combine(path, "films", films[filmNr] + ".mpg"));
+		}
+
+
+		static void DisplayList (IDictionary<string,string> begrippen)
+		{
+			foreach (var begrip in begrippen.OrderBy (b => b.Key))
+				Console.WriteLine (begrip.Key);
+		}
+	}
+
+	class IniReader
+	{
+		string path;
+
+		public IniReader (string path)
+		{
+			this.path = path;
+		}
+
+		public IEnumerable<string[]> Read(string fileName)
+		{
+			return File.ReadAllLines(Path.Combine(this.path, fileName)).SkipWhile (w => w.Trim () != "[Data]").Skip (1).Select (b => b.Split ('='));
 		}
 	}
 }
